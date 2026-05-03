daaecd854d853363127e7b113e1f55ef89aba3ac
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-05-03 18:27:44 +0530

    Update MySQL connection environment variables

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ app.use(express.json());
 
 // DB connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   ssl: {
     rejectUnauthorized: false
   }
